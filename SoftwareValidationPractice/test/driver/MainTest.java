c8b4f3f4a0625c7eae51113027327b900ab19f2b
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2017-09-16 18:20:17 -0400

    First version of project.

---
+++ SoftwareValidationPractice/test/driver/MainTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package driver;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author shamay
+ *
+ */
+public class MainTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {}
+
+  /**
+   * Test method for {@link driver.Main#main(java.lang.String[])}.
+   */
+  @Test
+  public void testMain() {
+    fail("Not yet implemented");
+  }
+
+}
